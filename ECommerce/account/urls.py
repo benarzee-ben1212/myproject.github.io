383775f1b21831e2cdf1d45cab15b99e200f3787
benarzee-ben1212 <90883894+benarzee-ben1212@users.noreply.github.com>
2024-02-19 15:11:52 +0530

    all

---
+++ ECommerce/account/urls.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+from .forms import LoginForm
+
+#new editing---------------------------------------------------------------
+
+
+app_name = 'account'
+
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='account/login.html', form_class=LoginForm), name='login')
+
+]
